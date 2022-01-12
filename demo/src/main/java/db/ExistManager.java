713a9a68a761b2443c3e08ecc360243c69626e03
Blizzsuxx <draganarsic1999@hotmail.com>
2022-01-12 15:11:43 +0100

    had to delete repo

--- demo/src/main/java/db/ExistManager.java
+++ demo/src/main/java/db/ExistManager.java
@@ -1,17 +1,17 @@
 package db;
 
 import java.io.File;
-import java.util.Collection;
 
 import javax.xml.transform.OutputKeys;
 
-import org.apache.catalina.util.ResourceSet;
 import org.exist.xmldb.EXistResource;
 import org.exist.xupdate.XUpdateProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.Database;
+import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.CollectionManagementService;
 import org.xmldb.api.modules.XMLResource;
