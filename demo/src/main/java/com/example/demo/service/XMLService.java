3b28c7b1594f395c32736a82d9b34695c1883512
nenadicu <urosnenadic18@gmail.com>
2022-01-12 17:30:28 +0100

    Dodati metodi za update, samo treba popraviti JSON koji se prosledjuje

--- demo/src/main/java/com/example/demo/service/XMLService.java
+++ demo/src/main/java/com/example/demo/service/XMLService.java
@@ -6,6 +6,11 @@ import com.example.demo.dto.XMLDto;
 import com.example.demo.jaxb.JaxB;
 import com.example.demo.model.interesovanje.InteresovanjeZaVakcinisanje;
 
+import com.example.demo.model.izvestajOImunizaciji.IzvestajOImunizaciji;
+import com.example.demo.model.potvrdaOVakcinaciji.PotvrdaOVakcinaciji;
+import com.example.demo.model.saglasnost.SaglasnostZaSprovodjenjeImunizacije;
+import com.example.demo.model.zahtevZaSertifikat.ZahtevZaSertifikat;
+import com.example.demo.model.zeleniSertifikat.DigitalniSertifikat;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,16 +22,63 @@ public class XMLService {
         this.jaxB = jaxB;
     }
 
-    public String jaxBTestInteresovanje(XMLDto dto) throws Exception {
-        JAXBContext context = JAXBContext.newInstance(InteresovanjeZaVakcinisanje.class);
+//    public String jaxBTestZahtevSertifikat(XMLDto dto) throws Exception {
+//        JAXBContext context = JAXBContext.newInstance(ZahtevZaSertifikat.class);
+//
+//        ZahtevZaSertifikat zahtev = (ZahtevZaSertifikat) jaxB.unmarshall(ZahtevZaSertifikat.class, dto.getText());
+//        zahtev.setMesto("Novi Sad");
+//        System.out.println(zahtev);
+//
+//        return jaxB.marshall(ZahtevZaSertifikat.class, zahtev);
+//    }
+//
+//    public String jaxBInteresovanje(XMLDto dto) throws Exception {
+//        JAXBContext context = JAXBContext.newInstance(InteresovanjeZaVakcinisanje.class);
+//
+//        InteresovanjeZaVakcinisanje interesovanje = (InteresovanjeZaVakcinisanje) jaxB.unmarshall(InteresovanjeZaVakcinisanje.class, dto.getText());
+//        interesovanje.setLokacija("Novi Sad");
+//        System.out.println(interesovanje);
+//
+//        return jaxB.marshall(InteresovanjeZaVakcinisanje.class, interesovanje);
+//    }
+//
+//    public String jaxBTestIzvestaj(XMLDto dto) throws Exception {
+//        JAXBContext context = JAXBContext.newInstance(IzvestajOImunizaciji.class);
+//
+//        IzvestajOImunizaciji izvestaj = (IzvestajOImunizaciji) jaxB.unmarshall(IzvestajOImunizaciji.class, dto.getText());
+//        izvestaj.setDatoJeVakcina(2l);
+//        System.out.println(izvestaj);
+//
+//        return jaxB.marshall(IzvestajOImunizaciji.class, izvestaj);
+//    }
+//
+//    public String jaxBTestPotvrda(XMLDto dto) throws Exception {
+//        JAXBContext context = JAXBContext.newInstance(PotvrdaOVakcinaciji.class);
+//
+//        PotvrdaOVakcinaciji potvrda = (PotvrdaOVakcinaciji) jaxB.unmarshall(PotvrdaOVakcinaciji.class, dto.getText());
+//        potvrda.setQRCode("dakndkansfnajojfnaddfnas");
+//        System.out.println(potvrda);
+//
+//        return jaxB.marshall(PotvrdaOVakcinaciji.class, potvrda);
+//    }
+//
+//    public String jaxBTestSaglasnost(XMLDto dto) throws Exception {
+//        JAXBContext context = JAXBContext.newInstance(SaglasnostZaSprovodjenjeImunizacije.class);
+//
+//        SaglasnostZaSprovodjenjeImunizacije saglasnost = (SaglasnostZaSprovodjenjeImunizacije) jaxB.unmarshall(SaglasnostZaSprovodjenjeImunizacije.class, dto.getText());
+//        saglasnost.setPotpis("gandas");
+//        System.out.println(saglasnost);
+//
+//        return jaxB.marshall(DigitalniSertifikat.class, saglasnost);
+//    }
 
+    public String jaxBTestZeleni(XMLDto dto) throws Exception {
+    JAXBContext context = JAXBContext.newInstance(DigitalniSertifikat.class);
 
-        InteresovanjeZaVakcinisanje interesovanje = (InteresovanjeZaVakcinisanje) jaxB.unmarshall(InteresovanjeZaVakcinisanje.class, dto.getText());
-        interesovanje.setLokacija("Novi Sad");
-        System.out.println(interesovanje);
+        DigitalniSertifikat sertifikat = (DigitalniSertifikat) jaxB.unmarshall(DigitalniSertifikat.class, dto.getText());
+        sertifikat.setQRCode("dakndkansfnajojfnaddfnas");
+        System.out.println(sertifikat);
 
-        return jaxB.marshall(InteresovanjeZaVakcinisanje.class, interesovanje);
-
-
+    return jaxB.marshall(DigitalniSertifikat.class, sertifikat);
     }
 }
