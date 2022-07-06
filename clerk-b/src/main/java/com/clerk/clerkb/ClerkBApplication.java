5ff9c71138cf5932c29e9019679fa797ff0505e2
marinagusa <marina.gusa.99@gmail.com>
2022-07-06 17:58:06 +0200

    Initialize client and server side projects

---
+++ clerk-b/src/main/java/com/clerk/clerkb/ClerkBApplication.java
@@ -0,0 +1,13 @@
+package com.clerk.clerkb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClerkBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClerkBApplication.class, args);
+	}
+
+}
