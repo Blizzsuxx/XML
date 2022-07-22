37ca18b90535039273a35ad9ebb6f9a0eb430b1c
marinagusa <marina.gusa.99@gmail.com>
2022-07-22 16:25:15 +0200

    Test

--- clerk-b/src/main/java/com/clerk/clerkb/controller/DigitalCertificateController.java
+++ clerk-b/src/main/java/com/clerk/clerkb/controller/DigitalCertificateController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "cert", produces = MediaType.APPLICATION_XML_VALUE)
 public class DigitalCertificateController {
 
+
     @Autowired
     private IDigitalCertificateService service;
 
