057f972c26ae66fc09cb24f86dee20817d771386
Blizzsuxx <draganarsic1999@hotmail.com>
2022-08-20 12:19:46 +0200

    popravio pozive na backend

--- frontend-clerk/src/app/services/profile-service.service.ts
+++ frontend-clerk/src/app/services/profile-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,26 +10,44 @@ export class ProfileServiceService {
   constructor(private http: HttpClient) { }
 
   public findAllRequests() {
-    return this.http.get(`${environment.apiUrl}/cert/requests`, {responseType: 'text'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/xml' });
+    headers.append('Accept', 'application/xml');
+    headers.append('Content-Type', 'application/xml');
+    return this.http.get(`${environment.apiUrl}/cert/requests`, {responseType: 'text', headers: headers});
   }
 
   public findAllCitizenDocuments(citizenId: string){
-    return this.http.get(`${environment.apiUrl}/cert/all-citizen/${citizenId}`, {responseType: 'text'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/xml' });
+    headers.append('Accept', 'application/xml');
+    headers.append('Content-Type', 'application/xml');
+    return this.http.get(`${environment.apiUrl}/cert/all-citizen/${citizenId}`, {responseType: 'text', headers: headers});
   }
 
   public acceptCertificateRequest(citizenId: string){
-    return this.http.post(`${environment.apiUrl}/cert/approve/${citizenId}`, {}, {responseType: 'text'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/xml' });
+    headers.append('Accept', 'application/xml');
+    headers.append('Content-Type', 'application/xml');
+    return this.http.post(`${environment.apiUrl}/cert/approve/${citizenId}`, {}, {responseType: 'text', headers: headers});
   }
 
   public declineCertificateRequest(citizenId: string, reason: string){
-    return this.http.post(`${environment.apiUrl}/cert/decline/${citizenId}/${reason}`, {});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/xml' });
+    headers.append('Accept', 'application/xml');
+    headers.append('Content-Type', 'application/xml');
+    return this.http.post(`${environment.apiUrl}/cert/decline/${citizenId}/${reason}`, {responseType: 'text', headers: headers});
   }
 
   public showDocument(documentId: string){
-    return this.http.get(`${environment.apiUrl}/cert/transform-interesovanje/${documentId}`, {responseType: 'text'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/xml' });
+    headers.append('Accept', 'application/xml');
+    headers.append('Content-Type', 'application/xml');
+    return this.http.get(`${environment.apiUrl}/cert/transform-interesovanje/${documentId}`, {responseType: 'text', headers: headers});
   }
 
   public createReport(start:string, end:string){
-    return this.http.get(`${environment.apiUrl}/immunization/report/${start}/${end}`, {responseType: 'text'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/xml' });
+    headers.append('Accept', 'application/xml');
+    headers.append('Content-Type', 'application/xml');
+    return this.http.get(`${environment.apiUrl}/immunization/report/${start}/${end}`, {responseType: 'text', headers: headers});
   }
 }
